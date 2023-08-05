b3f721eed1eb333f76d5fda3567781d480dc6a28
h4wk590 <aidanbrownj@gmail.com>
2023-08-05 15:19:59 -0700

    Updating Code

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-## Crypot Price Discord Webhook
+## Crypo Price Discord Webhook
+
+WIP
+
+- Sends Updated BTC price to discord as a webhook
+
+### Currently Working On
+
+- Containerizing the app
+- Cron for scheduling
+
--- src/main.py
+++ src/main.py
@@ -1,37 +1,36 @@
 import requests
+from dotenv import load_dotenv
 from bs4 import BeautifulSoup
-from datetime import datetime
-import time
+import discord
+import os
 
-while(True):
-    now = datetime.now()
-      
-    # this is just to get the time at the time of
-    # web scraping
-    current_time = now.strftime("%H:%M:%S")
-    print(f'At time : {current_time} IST')
-  
-    response = requests.get('https://finance.yahoo.com/cryptocurrencies/')
-    text = response.text
-    html_data = BeautifulSoup(text, 'html.parser')
-    headings = html_data.find_all('tr')[0]
-    headings_list = []
-    for x in headings:
-        headings_list.append(x.text)
-    headings_list = headings_list[:10]
-  
-    data = []
-  
-    for x in range(1, 5):
-        row = html_data.find_all('tr')[x]
-        column_value = row.find_all('td')
-        dict = {}
-          
-        for i in range(10):
-            dict[headings_list[i]] = column_value[i].text
-        data.append(dict)
-          
-    for coin in data:
-        print(coin)
-        print('')
-    time.sleep(600)
+
+# Your Discord webhook URL
+WEBHOOK_URL = os.getenv("WEBHOOK")
+
+def get_bitcoin_price():
+    url = 'https://www.binance.com/en/markets/overview'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    price_element = soup.find('div', class_='html body div#__APP div.css-ph4aey div.css-tq0shg main.css-1wr4jig div.css-9jfzzo div.css-194m5n4 div.css-1m4ys10 div#tabContainer.css-6g4rnu div.css-72ldqd div.css-dfdqsv div.css-1y8kib5 div.css-vurnku div.css-1pysja1 div.css-vlibs4 div.css-1iegfwm div.css-hwo5f4 div.css-ovtrou')
+    if price_element:
+        return price_element.text.strip()
+    else:
+        return 'Unable to fetch the bitcoin price.'
+
+def send_discord_webhook(message):
+    data = {
+        'content': message
+    }
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    response = requests.post(WEBHOOK_URL, json=data, headers=headers)
+    if response.status_code == 204:
+        print("Webhook sent successfully.")
+    else:
+        print(f"Failed to send webhook. Status code: {response.status_code}")
+
+if __name__ == '__main__':
+    bitcoin_price = get_bitcoin_price()
+    send_discord_webhook(f"Today's Bitcoin price: {bitcoin_price}")
